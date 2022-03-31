4a160d57455c12ee41fcff11f7057d9c779dfeb7
matheus <anyonebr1@gmail.com>
2022-03-31 12:30:22 -0300

    Fixed bug on settings page where it didn't fill the entire screen

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/navbar/Navbar";
+import React from "react";
 
 const Layout: React.FC = ({ children }) => {
   return (
-    <div className="flex flex-col bg-pastel-amber text-gray-800 antialiased md:flex-row">
+    <div className="flex min-h-screen flex-col bg-pastel-amber text-gray-800 antialiased md:flex-row">
       <Navbar />
       <main className="flex w-full py-16 px-10">
         <Outlet />
